f9f47dd2e054353cab25d0e69cae6d1b24376ad8
HDK <105449114+Mr-HDK@users.noreply.github.com>
2024-11-23 05:56:45 +0100

    Update test_calcul.c

--- TP-1/tests/test_calcul.c
+++ TP-1/tests/test_calcul.c
@@ -3,11 +3,6 @@
 
 void test_addition(void) {
     TEST_ASSERT_EQUAL_INT(5, addition(2, 3));
-    TEST_ASSERT_EQUAL_INT(-1, addition(2, -3));
+    TEST_ASSERT_EQUAL_INT(17, addition(10, -7));
 }
 
-int main(void) {
-    UNITY_BEGIN();
-    RUN_TEST(test_addition);
-    return UNITY_END();
-}
